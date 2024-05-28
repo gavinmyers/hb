1895124d685c7510eaf2fe5d591cf01cba854058
gavinmyers <gavin@gavinm.com>
2024-05-28 17:58:37 -0500

    hooking up transmit

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,28 @@
-console.log('Hello World')
+import { Transmit } from '@adonisjs/transmit-client'
+
+const messageContainer = document.querySelector('#messages')
+const messageForm = document.querySelector('#message_form')
+const messageInput = document.querySelector('#message')
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const values = new FormData(e.currentTarget)
+  void fetch('/messages', { method: 'POST', body: values })
+
+  messageInput.value = ''
+})
+
+const transmit = new Transmit({
+  baseUrl: window.location.origin,
+})
+
+const subscription = transmit.subscription('chats/1')
+await subscription.create()
+
+subscription.onMessage(({ message }) => {
+  const element = document.createElement('p')
+  element.innerText = message
+
+  messageContainer.appendChild(element)
+})
--- app/start/routes.ts
+++ app/start/routes.ts
@@ -7,6 +7,11 @@
 |
 */
 
+import logger from '@adonisjs/core/services/logger'
 import router from '@adonisjs/core/services/router'
+const ChatsController = () => import('#controllers/chats_controller')
+const PagesController = () => import('#controllers/pages_controller')
+logger.info("Loaded")
 
-router.on('/').render('pages/home')
+router.get('/', [PagesController, 'home'])
+router.post('messages', [ChatsController, 'store']).as('submit.message')
